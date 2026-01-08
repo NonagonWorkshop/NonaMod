879bdd0a8dfa903bbcba6949530e91cd217f997a
StarkMist111960 <starkmist111960@gmail.com>
2026-01-07 21:11:35 -0700

    Fix configuration file path in installer script

--- installer.sh
+++ installer.sh
@@ -16,7 +16,7 @@ fi
 log "Starting MushM Installer"
 
 NONA_MOD_CONF="https://raw.githubusercontent.com/NonagonWorkshop/Nonamod/main/utils/nonamod.conf"
-CONF_DIV="/etc/init/nonamod.conf"
+CONF_DIR="/etc/init/nonamod.conf"
 CROSH="/usr/bin/crosh"
 MURK_DIR="/mnt/stateful_partition/murkmod"
 MUSHM_URL="https://raw.githubusercontent.com/NonagonWorkshop/Nonamod/main/utils/mushm.sh"
@@ -26,7 +26,7 @@ BOOT_SK_DIR="/usr/local/bin/bootmsg.sh"
 log "Installing Needed Things And Shit"
 mkdir -p "$MURK_DIR/plugins" "$MURK_DIR/pollen" || error "Failed To Installing Needed Things And Shit"
 touch "$CONF_DIV"
-curl -fsSLo "$CONF_DIV" "$NONA_MOD_CONF" || error "Failed to download Config"
+curl -fsSLo "$CONF_DIR" "$NONA_MOD_CONF" || error "Failed to download Config"
 
 log "Installing MushM"
 curl -fsSLo "$CROSH" "$MUSHM_URL" || error "Failed to download MushM"
