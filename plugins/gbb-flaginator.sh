7fd9605329bd310e00505aed31be3dee452452f8
JohnDoe <166081472+GamerRyker@users.noreply.github.com>
2026-03-04 10:58:14 -0700

    Update gbb-flaginator.sh

--- plugins/gbb-flaginator.sh
+++ plugins/gbb-flaginator.sh
@@ -1,1 +1,8 @@
+#!/bin/bash
+# menu_plugin
+PLUGIN_NAME="GBB Flag-inator"
+PLUGIN_FUNCTION="Set your GBB flags"
+PLUGIN_DESCRIPTION="Edit your GBB flags and set them to a specific order to allow specific things"
+PLUGIN_AUTHOR="BinBashBanana"
+PLUGIN_VERSION=1
 
