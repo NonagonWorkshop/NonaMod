145c158761129b1afb563d5118d41dad4e258f1a
Star_destroyer11 <stardestroyersd89@gmail.com>
2026-03-05 08:47:33 -0700

    Add Python test plugin with basic functionality

---
+++ plugins/python_test_plugin.py
@@ -0,0 +1,11 @@
+PLUGIN_NAME="Test Python Plugin"
+PLUGIN_FUNCTION="test_python_plugin"
+PLUGIN_DESCRIPTION="A simple test plugin that prints a message."
+PLUGIN_AUTHOR="Star"
+PLUGIN_VERSION="1.0"
+
+def test_python_plugin():
+    print("Python plugin Working")
+
+if __name__ == "__main__":
+    test_python_plugin()
