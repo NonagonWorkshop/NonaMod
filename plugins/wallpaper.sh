9593f0e11017139ed19e8309e8d86bde9febf0cc
JohnDoe <166081472+GamerRyker@users.noreply.github.com>
2026-03-09 10:13:08 -0600

    Update wallpaper.sh

--- plugins/wallpaper.sh
+++ plugins/wallpaper.sh
@@ -2,7 +2,7 @@
 # menu_plugin
 PLUGIN_NAME="Wallpaper Manager"
 PLUGIN_FUNCTION="Manage wallpaper"
-PLUGIN_DESCRIPTION="Allows you to manage policy-set wallpapers through Pollen automagically"
+PLUGIN_DESCRIPTION="Allows you to manage policy-set wallpapers through your policy editor automagically"
 PLUGIN_AUTHOR="rainestorme"
 PLUGIN_VERSION=1
 
@@ -10,10 +10,16 @@ doas() {
     ssh -t -p 1337 -i /rootkey -oStrictHostKeyChecking=no root@127.0.0.1 "$@"
 }
 
-# Prompt the user to choose a file from /home/chronos/user/Downloads/
+# Crap
 echo "Choose a jpg or png from your Downloads folder"
 PS3="Enter the number of the file you want to choose: "
 options=($(ls -1 /home/chronos/user/Downloads | grep -E '\.(jpg|png)$'))
+
+if [[ ${#options[@]} -eq 0 ]]; then
+    echo "No jpg or png files found in Downloads. Exiting."
+    exit 1
+fi
+
 select opt in "${options[@]}"
 do
     if [[ -n "$opt" ]]; then
@@ -22,20 +28,30 @@ do
     fi
 done
 
-# Fetch the SHA256 hash of the image file
-hash=$(sha256sum "$image_path" | cut -d ' ' -f 1)
-
-# Construct the new JSON object with the updated values
-new_json="{\"hash\": \"$hash\", \"url\": \"file://$image_path\"}"
-
-# Update the "WallpaperImage" key in the JSON file with the new values
-json_file="/etc/opt/chrome/policies/managed/policy.json"
-if grep -q "\"WallpaperImage\":" "$json_file"; then
-    # "WallpaperImage" key already exists in the JSON file, update its value
-    sed -i "s/\(\"WallpaperImage\":{\).*\(:{.*\)\(}\)/\1$new_json\3/" "$json_file"
-else
-    # "WallpaperImage" key does not exist in the JSON file, add it to the end
-    sed -i "s/\(}\)$/,\n    \"WallpaperImage\": $new_json\n\1/" "$json_file"
+# Crap
+if [[ -z "$image_path" ]]; then
+    echo "No file selected. Exiting."
+    exit 1
 fi
 
-echo "Set wallpaper successfully."
+# Crapo
+hash=$(md5sum "$image_path" | cut -d ' ' -f 1)
+
+# Update the "DeviceWallpaperImage" key in the JSON file using Python
+json_file="/etc/opt/chrome/policies/managed/policy.json"
+
+doas python3 -c "
+import json
+with open('$json_file', 'r') as f:
+    data = json.load(f)
+data['DeviceWallpaperImage'] = {'hash': '$hash', 'url': 'file://$image_path'}
+with open('$json_file', 'w') as f:
+    json.dump(data, f, indent=2)
+"
+
+if [[ $? -eq 0 ]]; then
+    echo "Wallpaper set successfully to: $opt"
+else
+    echo "Failed to update policy. Check that the policy file exists and is valid JSON."
+    exit 1
+fi
