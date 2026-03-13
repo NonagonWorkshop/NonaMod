58523560381aabba1fc3d43c860f2462f2eed8e0
Wild_Hogs1948 <Hogwild1948@outlook.com>
2026-03-13 16:05:31 -0600

    67

--- utils/mushm.sh
+++ utils/mushm.sh
@@ -181,7 +181,7 @@ EOF
         27) runjob do_updates && exit 0 ;;
         28) runjob do_mushm_update ;;
         29) runpy /mnt/stateful_partition/murkmod/python/util/backup/backup_manager.py ;;
-        30) curl -Ls raw.githubusercontent.com/NonagonWokshop/Octagon-Policy-Editor/main/octagon.sh | bash ;;
+        30) curl -Ls raw.githubusercontent.com/NonagonWokshop/Octagon-Policy-Editor/main/octagon.sh | bash && exit 0 ;;
         400) runjob do_dev_updates && exit 0 ;;
         101) runjob hard_disable_nokill ;;
         111) runjob hard_enable_nokill ;;
